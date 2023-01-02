807b4c8be6756686c68622d7db5dacedb6f04b9e
Caio Cascaes <caio.cascaes@gmail.com>
2023-01-02 08:27:32 +0000

    Bump dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [nubank/matcher-combinators "3.5.1"]
-                 [prismatic/schema "1.4.0"]
+                 [nubank/matcher-combinators "3.7.2"]
+                 [prismatic/schema "1.4.1"]
                  [ring/ring-core "1.9.6"]
-                 [ring/ring-defaults "0.3.3"]
+                 [ring/ring-defaults "0.3.4"]
                  [ring/ring-json "0.5.1"]
                  [ring/ring-jetty-adapter "1.9.6"]
                  [org.clojure/data.json "2.4.0"]
